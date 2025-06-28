4cef79335d92111a3ed4824f97198da77d625e9a
RELLORbby <chrisparnham2003@gmail.com>
2025-06-28 21:19:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const canvas = document.getElementById('canvas');
+const fragments = document.querySelectorAll('.fragment');
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.clientX / window.innerWidth - 0.5;
+  const y = e.clientY / window.innerHeight - 0.5;
+
+  fragments.forEach((frag, i) => {
+    const intensity = (i + 1) * 10;
+    frag.style.transform = `translate(${x * intensity}px, ${y * intensity}px)`;
+  });
+});
 
